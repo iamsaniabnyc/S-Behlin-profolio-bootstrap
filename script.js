3776babf590d84266a257c662277d23b3fd8ace6
iamsaniabnyc <saniab10@nycstudents.net>
2024-08-06 16:06:52 +0000

    Buit an age verication app for my home page

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console. log('Im Sania')
+// console. log('Im Sania')
+
+// var prompt = prompt('What is your name?')
+
+// console.log(prompt)
+
+
+function verifyAge (age) {
+var age = prompt('What is your age?');
+console.log(age)
+
+if (age < 18) {
+  alert('You are a minior!!')
+} else if (age >= 18 <= 60) {
+alert('You are an adult!!')
+}else {
+  alert('You are a senior citizen!!')
+}
+`  `}
